eac0f1edc8f21420fa641ee110a21071c1f4d7e2
Artus U <artusrock@hotmail.com>
2022-05-21 11:39:13 +0100

    Product app created

---
+++ products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from products.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ["__str__", "description", "price", "sale_price"]
+	search_fields = ["title", "description"]
+	list_filter = ["price", "sale_price"]
+	list_editable = ["sale_price"]
+	class Meta:
+		model = Product
+  
+admin.site.register(Product, ProductAdmin)
