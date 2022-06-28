7d8bf79bedde46095588c4e618144c82d8633e8b
Artus U <artusrock@hotmail.com>
2022-06-28 10:49:34 +0100

    added custom TagManager

--- tags/models.py
+++ tags/models.py
@@ -4,20 +4,36 @@ from products.models import Product
 from django.db.models.signals import pre_save, post_save
 from django.utils.text import slugify
 
+
+class TagQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+class TagManager(models.Manager):
+	def get_queryset(self):
+		return TagQuerySet(self.model, using=self._db)
+	
+	def all(self, *args, **kwargs):
+		return super(TagManager, self).all(*args, **kwargs).active()
+
+
 class Tag(models.Model):
-    title = models.CharField(max_length=120, unique=True)
-    slug = models.SlugField(unique=True)
-    products = models.ManyToManyField(Product, blank=True)
-    active = models.BooleanField(default=True)
-    
-    def __str__(self):
-        return self.title
-    
-    def get_absolute_url(self):
-        return reverse('tags:detail', kwargs={'slug': self.slug})
-    
+	title = models.CharField(max_length=120, unique=True)
+	slug = models.SlugField(unique=True)
+	products = models.ManyToManyField(Product, blank=True)
+	active = models.BooleanField(default=True)
+	
+	objects = TagManager()
+	
+	def __str__(self):
+		return self.title
+	
+	def get_absolute_url(self):
+		return reverse('tags:detail', kwargs={'slug': self.slug})
+	
 
 def tag_pre_save_receiver(sender, instance, *args, **kwargs):
+	instance.title = instance.title.lower()
 	if not instance.slug:
 		instance.slug = slugify(instance.title)
 		
