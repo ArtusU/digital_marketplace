f574f681720b821af5834242dbb09e61a5d2b7ad
Artus U <artusrock@hotmail.com>
2022-07-09 09:00:08 +0100

    csrf_token getToken()

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,16 @@
 import datetime
 
 from django.http import HttpResponse, JsonResponse, Http404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from django.shortcuts import render
 
 
+#@method_decorator(csrf_exempt, name='dispatch')
 class CheckoutTestView(View):
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        if request.method == 'POST':
             # raise Http404
             data = {
                 "works": True,
